0836dd0a7467e013c1cbdb335778056cd34dcf4e
WolfZardST <gianflor@espol.edu.ec>
2022-11-18 22:18:50 -0500

    Agregando segundo chart

--- proyecto02/js/charts.js
+++ proyecto02/js/charts.js
@@ -1,8 +1,17 @@
 
+let jobsData = []
+
 window.addEventListener('DOMContentLoaded', () => {
 
+    let ua_selector = document.getElementById("ua_selector");
+    let job_selector = document.getElementById("job_selector");
+
+    let scores_chart = newScoresChart();
+    let jobs_chart = newJobsChart();
+
     loadUrbanAreas();
-    attachScores();
+    attachScores(ua_selector, scores_chart);
+    attachJobs(ua_selector, job_selector, jobs_chart);
 
 });
 
@@ -10,14 +19,16 @@ let newScoresChart = () => {
     return new CanvasJS.Chart("scores-chart", {
 
         animationEnabled: true,
-    
+        willReadFrequently: true,
+        title:{
+            text: "Scores Out Of Ten"
+        },
         axisX:{
             interval: 1
         },
         axisY2:{
             interlacedColor: "rgba(31, 134, 65,.1)",
-            gridColor: "rgba(1,77,101,.1)",
-            title: "Score out of 10"
+            gridColor: "rgba(1,77,101,.1)"
         },
         data: [{
             type: "bar",
@@ -26,13 +37,31 @@ let newScoresChart = () => {
             color: "rgb(16, 100, 44)",
             dataPoints: []
         }]
-    })
+    });
 }
 
-let ua_selector = document.getElementById("ua_selector");
-let scores_chart = newScoresChart();
+let newJobsChart = () => {
+    return new CanvasJS.Chart("jobs-chart", {
+        animationEnabled: true,
+        willReadFrequently: true,
+        title:{
+            text: "Jobs Salaries Per Year"
+        },
+        axisY :{
+            valueFormatString: "#0,.",
+            suffix: "k"
+        },
+        axisX: {
+            title: "Percentiles"
+        },
+        toolTip: {
+            shared: true
+        },
+        data: jobsData
+    });
+}
 
-loadUrbanAreas = async () => {
+let loadUrbanAreas = async () => {
 
     let local_URL = "http://localhost:3000/api.local/urban_areas.json";
 
@@ -49,7 +78,7 @@ loadUrbanAreas = async () => {
     });
 }
 
-attachScores = () => {
+let attachScores = (ua_selector, scores_chart) => {
 
     ua_selector.addEventListener("change", async (event) => {
 
@@ -68,5 +97,61 @@ attachScores = () => {
         });
 
         scores_chart.render();
+
     });
+}
+
+let attachJobs = (ua_selector, job_selector, jobs_chart) => {
+
+    ua_selector.addEventListener("change", async (event) => {
+
+        jobs_chart.destroy();
+        jobsData = [];
+        jobs_chart = newJobsChart();
+
+        let api_URL = event.target.value.concat("salaries/")
+
+        let response = await fetch(api_URL);
+        let data = await response.json();
+
+        let jobs = data["salaries"];
+        job_selector.innerHTML = "<option disabled selected>Pick a job to display</option>";
+
+        let percentiles = null;
+
+        jobs.forEach(job => {
+
+            percentiles = job["salary_percentiles"];
+
+            job_selector.innerHTML += 
+            `
+            <option value='{"title":" ${job["job"].title}", "p_25": ${percentiles.percentile_25}, "p_50": ${percentiles.percentile_50}, "p_75": ${percentiles.percentile_75}}'>${job["job"].title}</option>
+            `
+        });
+
+    });
+
+    job_selector.addEventListener("change", (event) => {
+
+        jobs_chart.destroy();
+
+        let job = JSON.parse(event.target.value);
+
+        let jobData = {        
+            type: "stackedArea",
+            showInLegend: true,
+            name: `${job.title}`,
+            dataPoints: [
+            { x: 25, y: job.p_25 },
+            { x: 50, y: job.p_50 },
+            { x: 75, y: job.p_75 }
+            ]
+        }
+        
+        jobsData.push(jobData);
+
+        jobs_chart = newJobsChart();
+        jobs_chart.render();
+    });
+
 }
